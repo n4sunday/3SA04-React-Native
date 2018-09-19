8566a4c8389fd17ba4cd16f8ca63278e7c5377a1
s5935512004 <s5935512004@phuket.psu.ac.th>
2018-09-20 02:04:01 +0700

    Update 1.1 Hello World

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
 export default class App extends React.Component {
+  doIt = () => {
+    console.log("Hello from console")
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text onPress={this.doIt}>Hello World</Text>
       </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
